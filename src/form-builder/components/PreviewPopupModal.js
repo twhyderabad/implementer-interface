66877603e6c4feb5e8766a333c61b34480ee0c15
Alekhya <alekhyayalla23@gmail.com>
2020-03-16 12:09:01 +0530

    Alekhya, Swetha | FBE-30 | Access methods in form controls

--- src/form-builder/components/PreviewPopupModal.js
+++ src/form-builder/components/PreviewPopupModal.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container as Container} from 'bahmni-form-controls';
+import  'bahmni-form-controls/dist/helpers.js';
 
 export default class PreviewPopupModal extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+        formData: {},
+        observations:{observations: []}};
     this.save = this.save.bind(this);
+    this.updateForm = this.updateForm.bind(this);
   }
 
     save() {
-        const form = this.props.formData;
+        const metadata = JSON.parse(this.props.formData.resources[0].value);
+        const form = {
+            component: {
+                state: {
+                    data: this.state.formData
+                },
+                props:{
+                    patient: undefined
+                }
+            },
+            events: {
+                onFormSave: metadata.events.onFormSave
+            }
+        };
+        this.setState({formData: runScript(this.state.formData)});
+    }
+
+    updateForm(updatedForm, obs) {
+        this.setState({formData: updatedForm});
+        this.setState({observations: obs});
     }
 
   render() {
     if (this.props.showPreview && this.props.formData) {
         const metadata = JSON.parse(this.props.formData.resources[0].value);
         metadata.version=this.props.formData.version;
-        console.log(" Preview modal ",metadata);
-
         return (
         <div>
           <div className="dialog preview-container">
@@ -28,9 +50,10 @@ export default class PreviewPopupModal extends React.Component {
               collapse={false}
               validate={false}
               translations={undefined}
-              observations={[]}
+              observations={this.state.observations.observations}
               patient={undefined}
               validateForm={false}
+              updateForm={this.updateForm}
               />
             </div>
             <button className="btn preview-close-btn"
