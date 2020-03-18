b1286d26721cc988b371e5a05c912075395dfa82
swetham <swetham@thoughtworks.com>
2020-03-18 23:04:23 +0530

    swetha | testing git hook | push should stop and exit

--- src/form-builder/components/ControlReduxWrapper.jsx
+++ src/form-builder/components/ControlReduxWrapper.jsx
@@ -198,9 +198,9 @@ export class ControlWrapper extends Draggable {
     if (properties && properties.property &&
       (properties.id === this.metadata.id && properties.property.controlEvent)) {
       return (
-        <Popup className="form-event-popup" closeOnDocumentClick={false}
+        <Popup className="form-event-popup"
           open={properties.id === this.metadata.id && properties.property.controlEvent}
-          position="top center"
+          position="top center" closeOnDocumentClick={false}
         >
         <ScriptEditorModal
           close={() => this.closeScriptEditorDialog(properties.id)}
